8373eed0227eb272bba7df83f04920d3ff2af451
Michael <michael_lau@live.co.uk>
2020-08-15 23:15:32 +0100

    CHANGE: Refactor code into a class

--- js/carousel.js
+++ js/carousel.js
@@ -1,142 +1,168 @@
-let currentIndex = 0;
-let visibleProducts = 3;
-const dataPath = '/data/recommendations.json';
-let recommendationsData = [];
-let productsData = [];
+class CustomCarousel {
+  currentIndex;
+  visibleProducts;
+  dataPath;
+  recommendationsData;
+  productsData;
 
-// setInterval(() => {
-
-// }, 10);
-
-buildCarousel = () => {
-  const carousel_left_button = document.querySelector(
-    '.carousel__button--left'
-  );
-  const carousel_right_button = document.querySelector(
-    '.carousel__button--right'
-  );
-  carousel_right_button.addEventListener('click', scrollRight);
-  carousel_left_button.addEventListener('click', scrollLeft);
-};
-
-generateProduct = (recommendation) => {
-  const div = document.createElement('div');
-  const product_image = document.createElement('img');
-  const product_name = document.createElement('h3');
-  const product_price = document.createElement('h3');
-  div.classList.add('carousel__product');
-  // console.log(recommendation.imageSrc);
-  product_image.src = recommendation.imageSrc;
-  // product_image.src =
-  //   'https://www.spacenk.com/dw/image/v2/ABCE_PRD/on/demandware.static/-/Sites-spacenkmastercatalog/default/dw0371e163/products/OSKIA/UK200013165_OSKIA.jpg';
-  product_name.classList.add('heading-primary');
-  product_name.innerText = recommendation.productTitle;
-  product_price.innerText = recommendation.price;
-  product_price.classList.add('heading-primary');
-
-  div.appendChild(product_image);
-  div.appendChild(product_name);
-  div.appendChild(product_price);
-  return div;
-};
-
-appendProduct = (product) => {
-  const parent = document.querySelector('.carousel__body');
-  calculateProductPosition(product);
-  parent.appendChild(product);
-};
-
-appendAllProducts = (productsData) => {
-  for (let i = 0; i < productsData.length; i++) {
-    appendProduct(productsData[i]);
+  constructor() {
+    this.currentIndex = 0;
+    this.visibleProducts = 3;
+    this.dataPath = '/data/recommendations.json';
+    this.recommendationsData = []; // JSON data
+    this.productsData = []; // Array of products as <div> tags
   }
-};
 
-calculateProductPosition = (product) => {
-  const parent = document.querySelector('.carousel__body');
-  const number_of_children = parent.childElementCount;
-  const carousel_product_width = calculateProductWidth();
-  const carousel_product_offest = currentIndex * carousel_product_width;
-  const margin_value =
-    number_of_children * carousel_product_width + carousel_product_offest;
-  product.style = `margin-left: ${margin_value}px`;
-};
+  initializeCarousel = () => {
+    this.assignEvents();
+    this.getData();
+  };
 
-getData = () => {
-  fetch(dataPath)
-    .then((res) => res.json())
-    .then((data) => {
-      recommendationsData = data.productData;
-      console.log(recommendationsData);
-      for (let i = 0; i < recommendationsData.length; i++) {
-        productsData[i] = generateProduct(data.productData[i]);
+  //Add Event Listeners
+  assignEvents = () => {
+    const carousel_left_button = document.querySelector(
+      '.carousel__button--left'
+    );
+    const carousel_right_button = document.querySelector(
+      '.carousel__button--right'
+    );
+    carousel_right_button.addEventListener('click', this.scrollRight);
+    carousel_left_button.addEventListener('click', this.scrollLeft);
+  };
+
+  generateProduct = (recommendation) => {
+    const div = document.createElement('div');
+    const product_image = document.createElement('img');
+    const product_name = document.createElement('h3');
+    const product_price = document.createElement('h3');
+    div.classList.add('carousel__product');
+    product_image.src = recommendation.imageSrc;
+    product_name.classList.add('heading-primary');
+    product_name.innerText = recommendation.productTitle;
+    product_price.innerText = recommendation.price;
+    product_price.classList.add('heading-primary');
+
+    div.appendChild(product_image);
+    div.appendChild(product_name);
+    div.appendChild(product_price);
+    return div;
+  };
+
+  //Append a single product to the carousel body
+  appendProduct = (product) => {
+    const parent = document.querySelector('.carousel__body');
+    this.calculateProductPosition(product);
+    parent.appendChild(product);
+  };
+
+  //Append all products to the carousel body at once
+  appendAllProducts = (productsData) => {
+    for (let i = 0; i < productsData.length; i++) {
+      this.appendProduct(productsData[i]);
+    }
+  };
+
+  //Calculate where a product starts based on margin left styling
+  calculateProductPosition = (product) => {
+    const carousel_body = document.querySelector('.carousel__body');
+    const number_of_children = carousel_body.childElementCount;
+    const carousel_product_width = this.calculateProductWidth();
+    const carousel_product_offest = this.currentIndex * carousel_product_width;
+    const margin_value =
+      number_of_children * carousel_product_width + carousel_product_offest;
+    product.style = `margin-left: ${margin_value}px`;
+  };
+
+  calculateProductWidth = () => {
+    const carousel_body = document.querySelector('.carousel__body');
+    const carousel_product_width =
+      carousel_body.getBoundingClientRect().width / this.visibleProducts;
+    return carousel_product_width;
+  };
+
+  //Get data, then append all products to carousel body
+  getData = () => {
+    fetch(this.dataPath)
+      .then((res) => res.json())
+      .then((data) => {
+        this.recommendationsData = data.productData;
+        for (let i = 0; i < this.recommendationsData.length; i++) {
+          this.productsData[i] = this.generateProduct(data.productData[i]);
+        }
+      })
+      .then(() => {
+        this.appendAllProducts(this.productsData);
+      });
+  };
+
+  scrollRight = () => {
+    const carousel_product_list = document.querySelectorAll(
+      '.carousel__product'
+    );
+    const carousel_product_width = this.calculateProductWidth();
+    this.disableRightButton();
+
+    if (!(this.currentIndex >= this.productsData.length - 1)) {
+      for (let i = 0; i < carousel_product_list.length; i++) {
+        carousel_product_list[i].addEventListener(
+          'transitionend',
+          this.scrollEnd
+        );
+        const current_style = window.getComputedStyle(carousel_product_list[i]);
+        const current_margin_left = current_style.getPropertyValue(
+          'margin-left'
+        );
+        const new_margin_left =
+          parseInt(current_margin_left) - carousel_product_width;
+        carousel_product_list[i].style = `margin-left: ${new_margin_left}px`;
       }
-    })
-    .then(() => {
-      appendAllProducts(productsData);
-    });
-};
-
-scrollRight = () => {
-  const carousel_product_list = document.querySelectorAll('.carousel__product');
-  const carousel_product_width = calculateProductWidth();
-  disableRightButton();
-
-  if (!(currentIndex >= productsData.length - 1)) {
-    for (let i = 0; i < carousel_product_list.length; i++) {
-      carousel_product_list[i].addEventListener('transitionend', scrollEnd);
-      const current_style = window.getComputedStyle(carousel_product_list[i]);
-      const current_margin_left = current_style.getPropertyValue('margin-left');
-      const new_margin_left =
-        parseInt(current_margin_left) - carousel_product_width;
-      carousel_product_list[i].style = `margin-left: ${new_margin_left}px`;
+      this.currentIndex++;
     }
-    console.log(currentIndex);
-    currentIndex++;
-  }
-};
+  };
 
-scrollLeft = () => {
-  const carousel_product_list = document.querySelectorAll('.carousel__product');
-  const carousel_product_width = calculateProductWidth();
-  disableLeftButton();
+  scrollLeft = () => {
+    const carousel_product_list = document.querySelectorAll(
+      '.carousel__product'
+    );
+    const carousel_product_width = this.calculateProductWidth();
+    this.disableLeftButton();
 
-  if (!(currentIndex <= 0)) {
-    for (let i = 0; i < carousel_product_list.length; i++) {
-      carousel_product_list[i].addEventListener('transitionend', scrollEnd);
-      const current_style = window.getComputedStyle(carousel_product_list[i]);
-      const current_margin_left = current_style.getPropertyValue('margin-left');
-      const new_margin_left =
-        parseInt(current_margin_left) + carousel_product_width;
-      carousel_product_list[i].style = `margin-left: ${new_margin_left}px`;
+    if (!(this.currentIndex <= 0)) {
+      for (let i = 0; i < carousel_product_list.length; i++) {
+        carousel_product_list[i].addEventListener(
+          'transitionend',
+          this.scrollEnd
+        );
+        const current_style = window.getComputedStyle(carousel_product_list[i]);
+        const current_margin_left = current_style.getPropertyValue(
+          'margin-left'
+        );
+        const new_margin_left =
+          parseInt(current_margin_left) + carousel_product_width;
+        carousel_product_list[i].style = `margin-left: ${new_margin_left}px`;
+      }
+      this.currentIndex--;
     }
-    currentIndex--;
-  }
-};
+  };
 
-scrollEnd = () => {
-  buildCarousel();
-};
+  scrollEnd = () => {
+    this.assignEvents();
+  };
 
-disableRightButton = () => {
-  const carousel_right_button = document.querySelector(
-    '.carousel__button--right'
-  );
-  carousel_right_button.removeEventListener('click', scrollRight);
-};
+  disableRightButton = () => {
+    const carousel_right_button = document.querySelector(
+      '.carousel__button--right'
+    );
+    carousel_right_button.removeEventListener('click', this.scrollRight);
+  };
 
-disableLeftButton = () => {
-  const carousel_left_button = document.querySelector(
-    '.carousel__button--left'
-  );
-  carousel_left_button.removeEventListener('click', scrollLeft);
-};
+  disableLeftButton = () => {
+    const carousel_left_button = document.querySelector(
+      '.carousel__button--left'
+    );
+    carousel_left_button.removeEventListener('click', this.scrollLeft);
+  };
+}
 
-calculateProductWidth = () => {
-  const carousel_body = document.querySelector('.carousel__body');
-  const carousel_product_width =
-    carousel_body.getBoundingClientRect().width / visibleProducts;
-  return carousel_product_width;
-};
-
-buildCarousel();
+let carousel = new CustomCarousel();
+carousel.initializeCarousel();
