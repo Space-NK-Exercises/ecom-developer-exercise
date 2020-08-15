93806f9cb923442d695ca3a979ae9f85d4f15b0c
Michael <michael_lau@live.co.uk>
2020-08-15 22:49:07 +0100

    ADD: Fetch Data from JSON functionality

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,12 @@
 let currentIndex = 0;
 let visibleProducts = 3;
+const dataPath = '/data/recommendations.json';
+let recommendationsData = [];
 let productsData = [];
+
+// setInterval(() => {
+
+// }, 10);
 
 buildCarousel = () => {
   const carousel_left_button = document.querySelector(
@@ -13,17 +19,19 @@ buildCarousel = () => {
   carousel_left_button.addEventListener('click', scrollLeft);
 };
 
-generateProduct = () => {
+generateProduct = (recommendation) => {
   const div = document.createElement('div');
   const product_image = document.createElement('img');
   const product_name = document.createElement('h3');
   const product_price = document.createElement('h3');
   div.classList.add('carousel__product');
-  product_image.src =
-    'https://www.spacenk.com/on/demandware.static/-/Sites-spacenkmastercatalog/default/dwc9059537/products/FRESH/UK200025595_FRESH.jpg';
+  // console.log(recommendation.imageSrc);
+  product_image.src = recommendation.imageSrc;
+  // product_image.src =
+  //   'https://www.spacenk.com/dw/image/v2/ABCE_PRD/on/demandware.static/-/Sites-spacenkmastercatalog/default/dw0371e163/products/OSKIA/UK200013165_OSKIA.jpg';
   product_name.classList.add('heading-primary');
-  product_name.innerText = 'Fresh - Soy Face Cleanser';
-  product_price.innerText = '30.00';
+  product_name.innerText = recommendation.productTitle;
+  product_price.innerText = recommendation.price;
   product_price.classList.add('heading-primary');
 
   div.appendChild(product_image);
@@ -34,33 +42,49 @@ generateProduct = () => {
 
 appendProduct = (product) => {
   const parent = document.querySelector('.carousel__body');
-  const number_of_children = parent.childElementCount;
-  const carousel_product_width = calculateProductWidth();
-  const margin_value = number_of_children * carousel_product_width;
-  console.log(carousel_product_width);
-  product.style = `margin-left: ${margin_value}px`;
+  calculateProductPosition(product);
   parent.appendChild(product);
 };
 
 appendAllProducts = (productsData) => {
-  getData();
   for (let i = 0; i < productsData.length; i++) {
     appendProduct(productsData[i]);
   }
 };
 
+calculateProductPosition = (product) => {
+  const parent = document.querySelector('.carousel__body');
+  const number_of_children = parent.childElementCount;
+  const carousel_product_width = calculateProductWidth();
+  const carousel_product_offest = currentIndex * carousel_product_width;
+  const margin_value =
+    number_of_children * carousel_product_width + carousel_product_offest;
+  product.style = `margin-left: ${margin_value}px`;
+};
+
 getData = () => {
-  for (let i = 0; i < 10; i++) {
-    productsData[i] = generateProduct();
-  }
+  fetch(dataPath)
+    .then((res) => res.json())
+    .then((data) => {
+      recommendationsData = data.productData;
+      console.log(recommendationsData);
+      for (let i = 0; i < recommendationsData.length; i++) {
+        productsData[i] = generateProduct(data.productData[i]);
+      }
+    })
+    .then(() => {
+      appendAllProducts(productsData);
+    });
 };
 
 scrollRight = () => {
   const carousel_product_list = document.querySelectorAll('.carousel__product');
   const carousel_product_width = calculateProductWidth();
+  disableRightButton();
 
   if (!(currentIndex >= productsData.length - 1)) {
     for (let i = 0; i < carousel_product_list.length; i++) {
+      carousel_product_list[i].addEventListener('transitionend', scrollEnd);
       const current_style = window.getComputedStyle(carousel_product_list[i]);
       const current_margin_left = current_style.getPropertyValue('margin-left');
       const new_margin_left =
@@ -75,9 +99,11 @@ scrollRight = () => {
 scrollLeft = () => {
   const carousel_product_list = document.querySelectorAll('.carousel__product');
   const carousel_product_width = calculateProductWidth();
+  disableLeftButton();
 
   if (!(currentIndex <= 0)) {
     for (let i = 0; i < carousel_product_list.length; i++) {
+      carousel_product_list[i].addEventListener('transitionend', scrollEnd);
       const current_style = window.getComputedStyle(carousel_product_list[i]);
       const current_margin_left = current_style.getPropertyValue('margin-left');
       const new_margin_left =
@@ -89,10 +115,21 @@ scrollLeft = () => {
 };
 
 scrollEnd = () => {
-  const scrollBody = document.querySelector('.carousel__body');
-  console.log(scrollBody);
-  scrollBody.removeChild(scrollBody.firstElementChild);
-  scrollBody.removeEventListener('transitionend', scrollEnd);
+  buildCarousel();
+};
+
+disableRightButton = () => {
+  const carousel_right_button = document.querySelector(
+    '.carousel__button--right'
+  );
+  carousel_right_button.removeEventListener('click', scrollRight);
+};
+
+disableLeftButton = () => {
+  const carousel_left_button = document.querySelector(
+    '.carousel__button--left'
+  );
+  carousel_left_button.removeEventListener('click', scrollLeft);
 };
 
 calculateProductWidth = () => {
