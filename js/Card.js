973927316d6c99a20ecdc5dea7814245caee193c
Dominic Myers <annoyingmouse@gmail.com>
2021-03-02 16:45:48 +0000

    CSS only technique - looks like it'll be a ballache, try something else

--- js/Card.js
+++ js/Card.js
@@ -11,16 +11,22 @@ export class Card {
   }
   render(target) {
     const markup = `
-      <a class="carousel-item" 
-         href="${this.productUrl}"
-         target="_blank"
-         data-position="${this.id}">
-        <img src="${this.imageSrc}"
-             alt="${this.productTitle}"/>
-        <h2>${this.productTitle}</h2>
-        <p>${this.price}</p>
-      </a>
+      <div class='carousel__item carousel__item--mobile-in-1 carousel__item--tablet-in-2 carousel__item--desktop-in-3'>
+        <div class='demo-content'>
+          <a href="${this.productUrl}"
+             target="_blank"
+             data-position="${this.id}">
+            <img src="${this.imageSrc}"
+                alt="${this.productTitle}"/>
+            <div>
+              <h2>${this.productTitle}</h2>
+              <p>${this.price}</p>
+            </div>
+          </a>
+        </div>
+      </div>
     `
-    target.insertAdjacentHTML("afterbegin", markup)
+    const track = target.querySelector(".carousel__track")
+    track.insertAdjacentHTML("afterbegin", markup)
   }
 }
