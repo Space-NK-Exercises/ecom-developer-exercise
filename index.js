ac83eb77128c97b57f2133347624e75e1007fbe0
partham789 <partham789@gmail.com>
2021-08-04 13:31:49 +0100

    Read JSON file

---
+++ index.js
@@ -0,0 +1,16 @@
+function getJSON(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+getJSON("./data/recommendations.json", function(json){
+    var data = JSON.parse(json);
+    console.log(data);
+});
