684dbb770eaa7a9c10fb572cf20895be0805df50
partham789 <partham789@gmail.com>
2021-08-04 15:27:16 +0100

    Create list of product element

--- index.js
+++ index.js
@@ -12,5 +12,34 @@ function getJSON(file, callback) {
 
 getJSON("./data/recommendations.json", function(json){
     var data = JSON.parse(json);
-    console.log(data);
+    createProductContainer(data);
 });
+
+
+
+function createProductContainer(data){
+    var product = data.productData;
+    for( var i = 0; i < product.length; i++) {
+        const productGrid = document.getElementsByClassName("product-grid");
+        const productContainer = document.createElement("div");
+        productContainer.classList.add("product-container");
+        const productElement = document.createElement("div");
+        const priceElement = document.createElement("span");
+        const titleElement = document.createElement("p");
+        const imageElement = document.createElement("img");
+        const brandElement = document.createElement("h3");
+        productElement.classList.add("product");
+        imageElement.src = product[i].imageSrc;
+        imageElement.alt = product[i].productTitle;
+        const brandAndTitle = product[i].productTitle.split("-");
+        brandElement.innerText = brandAndTitle[0].trim();
+        titleElement.innerText = brandAndTitle[1] ? brandAndTitle[1].trim() : null;
+        priceElement.innerHTML = product[i].price;
+        productElement.appendChild(imageElement);
+        productElement.appendChild(brandElement);
+        productElement.appendChild(titleElement);
+        productElement.appendChild(priceElement);
+        productContainer.appendChild(productElement);
+        productGrid[0].appendChild(productContainer);
+    }
+}
