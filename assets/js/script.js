716f10e1ea66747f0296c3d1808eb164e4d6c1c4
fahra akhlaq <fahraakhlaq@fahras-mbp.home>
2022-04-28 12:45:40 +0100

    able to distinguish index on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+const productData = [
+	{
+		productUrl:
+			'https://www.spacenk.com/uk/en_GB/skincare/cleansers/cleanser/soy-face-cleanser-MUK200025595.html',
+		imageSrc:
+			'https://www.spacenk.com/on/demandware.static/-/Sites-spacenkmastercatalog/default/dwc9059537/products/FRESH/UK200025595_FRESH.jpg',
+		productTitle: 'Fresh - Soy Face Cleanser',
+		price: '30.00',
+	},
+	{
+		productUrl:
+			'https://www.spacenk.com/uk/en_GB/skincare/cleansers/cleanser/cleanser-MUK154050009.html',
+		imageSrc:
+			'https://www.spacenk.com/on/demandware.static/-/Sites-spacenkmastercatalog/default/dw4fed2dd5/products/EVE_LOM/UK154050009_EVE_LOM.jpg',
+		productTitle: 'Eve Lom - Cleanser',
+		price: '85.00',
+	},
+];
+
 const previousBtn = document.querySelector('#previous');
 const nextBtn = document.querySelector('#next');
 
@@ -8,19 +27,25 @@ const showNext = () => {
 	console.log('next');
 };
 
-const constructCarouselItem = () => {
-	console.log('constructed');
+const handleRelocate = (event) => {
+	console.log('relocate to product page');
+
+	const productClicked = event.currentTarget.getAttribute('data-index');
+	console.log(productClicked);
+};
+
+const constructCarouselItem = (product, index) => {
 	// construct carousel item
 	const price = document.createElement('h3');
-	price.textContent = '£40.00';
+	price.textContent = product.price;
 
 	const productName = document.createElement('h4');
 	productName.setAttribute('class', 'product-name');
-	productName.textContent = 'Ultimate face wash';
+	productName.textContent = product.productTitle;
 
 	const brand = document.createElement('h3');
 	brand.setAttribute('class', 'brand-name');
-	brand.textContent = 'Amanda';
+	brand.textContent = product.productTitle;
 
 	const detailsDiv = document.createElement('div');
 	detailsDiv.setAttribute('class', 'carousel-item-details');
@@ -28,28 +53,28 @@ const constructCarouselItem = () => {
 
 	const productImg = document.createElement('img');
 	productImg.setAttribute('class', 'product-img');
-	productImg.setAttribute(
-		'src',
-		'https://static.thcdn.com/images/large/webp/productimg/1600/1600/11471638-8344484790681069.jpg'
-	);
+	productImg.setAttribute('src', product.imageSrc);
 
 	const itemDiv = document.createElement('div');
 	itemDiv.setAttribute('class', 'carousel-item');
+	itemDiv.setAttribute('data-index', index);
 	itemDiv.append(productImg, detailsDiv);
+
+	itemDiv.addEventListener('click', handleRelocate);
 
 	return itemDiv;
 };
 
 const renderCarouselItem = () => {
-	const carouselItem = constructCarouselItem();
+	const carouselItem = constructCarouselItem(productData[0], 0);
+	const carouselItem2 = constructCarouselItem(productData[1], 1);
 
 	// append to parent
 	const carousel = document.querySelector('#carousel');
-	carousel.append(carouselItem);
+	carousel.append(carouselItem, carouselItem2);
 };
 
 const onLoad = () => {
-	console.log('loaded');
 	renderCarouselItem();
 };
 
