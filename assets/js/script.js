b4953c9a26fc106567a0e7367945627ba48c4b34
fahra akhlaq <fahraakhlaq@fahras-mbp.home>
2022-04-28 12:02:02 +0100

    dynamically rendered carousel item

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,42 @@ const showNext = () => {
 
 const constructCarouselItem = () => {
 	console.log('constructed');
+	// construct carousel item
+	const price = document.createElement('h3');
+	price.textContent = '£40.00';
+
+	const productName = document.createElement('h4');
+	productName.setAttribute('class', 'product-name');
+	productName.textContent = 'Ultimate face wash';
+
+	const brand = document.createElement('h3');
+	brand.setAttribute('class', 'brand-name');
+	brand.textContent = 'Amanda';
+
+	const detailsDiv = document.createElement('div');
+	detailsDiv.setAttribute('class', 'carousel-item-details');
+	detailsDiv.append(brand, productName, price);
+
+	const productImg = document.createElement('img');
+	productImg.setAttribute('class', 'product-img');
+	productImg.setAttribute(
+		'src',
+		'https://static.thcdn.com/images/large/webp/productimg/1600/1600/11471638-8344484790681069.jpg'
+	);
+
+	const itemDiv = document.createElement('div');
+	itemDiv.setAttribute('class', 'carousel-item');
+	itemDiv.append(productImg, detailsDiv);
+
+	return itemDiv;
 };
 
 const renderCarouselItem = () => {
-	constructCarouselItem();
-	console.log('carousel item');
-	// construct carousel item
+	const carouselItem = constructCarouselItem();
 
 	// append to parent
+	const carousel = document.querySelector('#carousel');
+	carousel.append(carouselItem);
 };
 
 const onLoad = () => {
