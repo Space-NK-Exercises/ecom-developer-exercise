ca07c29b711893dc7e9b9ef102bea9442f3d3b52
fahra akhlaq <fahraakhlaq@fahras-mbp.home>
2022-04-28 11:21:56 +0100

    functions passing on functions correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,5 +8,23 @@ const showNext = () => {
 	console.log('next');
 };
 
+const constructCarouselItem = () => {
+	console.log('constructed');
+};
+
+const renderCarouselItem = () => {
+	constructCarouselItem();
+	console.log('carousel item');
+	// construct carousel item
+
+	// append to parent
+};
+
+const onLoad = () => {
+	console.log('loaded');
+	renderCarouselItem();
+};
+
 previousBtn.addEventListener('click', showPrevious);
 nextBtn.addEventListener('click', showNext);
+window.addEventListener('load', onLoad);
